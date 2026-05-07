e28027656f706fec9bfb05df954bc16769ff661b
Juan8116 <bg1373@est.ort.edu.ar>
2026-05-07 10:36:03 -0300

    destroy player

---
+++ Assets/Scripts/PlayerCollisionDetection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Caja"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
